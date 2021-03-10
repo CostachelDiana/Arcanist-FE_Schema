ba2cf6abcee4b4c8b95c568d4f294300ff55cf9f
cspataru <catalin.spataru@verint.com>
2021-03-10 21:18:56 +0200

    Temporary fix of modal closing

--- src/app/firstpage/firstpage.component.ts
+++ src/app/firstpage/firstpage.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import {BackendAPIHandler,IBEApiConsumer} from "../common/BackendAPIHandler"
 import {CreateProjectDialogue} from "../dialogues/CreateProjectDialogue.component"
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog'; 
+import * as $ from 'jquery';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-project',
@@ -72,8 +74,12 @@ export class FirstPage implements OnInit, IBEApiConsumer {
   {
 	  if (evtType=="capture-uploaded")
 	  {
-		  var capID = jObj["uuid"];		  
-		  this.router.navigateByUrl("/CaptureEdit?capid="+capID);
+      alert("Capture uploaded successfully");
+
+		  var capID = jObj["uuid"];
+
+      $('#closeModal').click;
+
 		  this.isCaptureUploading=false;
 	  } else if (evtType=="project-created")
 	  {
